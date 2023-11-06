fb155df5f672e4c4cb7239418fd6df54580fc559
Sergio Yalux <randaledomman1@gmail.com>
2023-11-06 11:34:38 -0300

    fix(backend): set database timezone to Argentina

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -9,6 +9,7 @@ const pool = mysql.createPool({
     user: environment.DB.USER,
     password: environment.DB.PASS,
     database: environment.DB.NAME,
+    timezone: 'America/Argentina/Buenos_Aires',
 });
 
 const db = { pool };
